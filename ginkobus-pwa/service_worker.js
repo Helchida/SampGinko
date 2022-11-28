daca86507dd862865d32933eb307de23622f674f
mlecle11 <morgan.leclercq@edu.univ-fcomte.fr>
2022-11-28 17:44:44 +0100

    add other files

---
+++ ginkobus-pwa/service_worker.js
@@ -0,0 +1,66 @@
+        
+const cacheName = 'cache';
+const appShellFiles = [ 
+
+            "icons/favicon.ico",
+            "icons/icon-32.png",
+            "icons/icon-64.png",
+            "icons/icon-96.png",
+            "icons/icons-128.png",
+            "icons/icons-168.png",
+            "icons/icon-180.png",
+            "icons/icon-192.png",
+            "icons/icon-256.png",
+            "icons/icon-512.png",
+            "icons/maskable_icon.png",
+            "app.js",
+            "index.html",
+            "style.css"
+];
+
+
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+
+    e.waitUntil((async () => {
+        const cache = await caches.open(cacheName);
+        console.log('[Service Worker] Caching all: app shell and content');
+        await cache.addAll(appShellFiles);
+      })());
+
+});
+
+
+
+self.addEventListener('fetch', (e) => {
+
+    self.addEventListener('fetch', function (event) {
+        event.respondWith(
+          caches.open('mysite-dynamic').then(function (cache) {
+            return cache.match(event.request).then(function (response) {
+              return (
+                response ||
+                fetch(event.request).then(function (response) {
+                  cache.put(event.request, response.clone());
+                  return response;
+                })
+              );
+            });
+          }),
+        );
+      });
+
+    /*
+    e.respondWith((async () => {
+      const r = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (r) { return r; }
+      const response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+    */
+  });
+  
